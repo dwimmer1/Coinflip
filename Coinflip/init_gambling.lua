80f0e774734373a9e26e0c596ee6801e012a7412
ThatDani <67701825+ThatDani@users.noreply.github.com>
2020-08-09 16:26:55 +0200

    Update init_gambling.lua

--- Coinflip/init_gambling.lua
+++ Coinflip/init_gambling.lua
@@ -5,7 +5,6 @@ AddCSLuaFile("cl_gambling.lua")
 
 hook.Add("PlayerSay", "StartCommand", function(ply, text, ent)
     if text == "!g" then
-        --print("jooooooooooo")
         net.Start("start")
         net.Send(ply)
     end
@@ -17,9 +16,7 @@ function ExchangeInit()
         local ButtonPressCheck = net.ReadBool() --Prüft ob der Button gedrückt wurde
 
         if (ButtonPressCheck ~= true) then
-            --local ExchangeNumber = InputEndNumberInit
             print(InputEndNumberInit .. "")
-            print("Jooooooo funkt")
 
             if not ply:canAfford(InputEndNumberInit) then
                 DarkRP.notify(ply, 1, 4, string.format("Das kannst du dir nicht leisten."))
