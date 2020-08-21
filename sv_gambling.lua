3e4c3b302916b3d350c9edd973278de111a40494
ThatDani <daniel.wimmer00041@gmail.com>
2020-08-21 20:59:35 +0200

    Changes
    New Network, Part for TokensToCash

--- sv_gambling.lua
+++ sv_gambling.lua
@@ -2,6 +2,7 @@
 util.AddNetworkString("start")
 util.AddNetworkString("Exchange")
 util.AddNetworkString("BackToken")
+util.AddNetworkString("ExchangeToCash")
 AddCSLuaFile("cl_gambling.lua")
 
 hook.Add("PlayerSay", "StartCommand", function(ply, text, ent)
@@ -14,21 +15,32 @@ end)
 
 function ExchangeInit()
     net.Receive("Exchange", function(len, ply)
-        local InputEndNumberInit = net.ReadUInt(8)
-        local ButtonPressCheck = net.ReadBool() --Prüft ob der Button gedrückt wurde
-        local CurrCash = ply:getDarkRPVar("money")
-        local TokenBoughtValue = InputEndNumberInit * 10
-        if (ButtonPressCheck == false) then
+        --local CurrTokens = net.ReadUInt(12)
+        local InputEndToTokensNumber = net.ReadUInt(8) -- = Box Input
+        local TokenBoughtValue = InputEndToTokensNumber * 10
 
-            if not ply:canAfford(InputEndNumberInit) then
-                DarkRP.notify(ply, 1, 4, string.format("Das kannst du dir nicht leisten."))
-            else
-                ply:addMoney(-InputEndNumberInit)
-                net.Start("BackToken")
-                net.WriteUInt(TokenBoughtValue, 8)
-                DarkRP.notify(ply, 0, 4, string.format("Du hast dir für " .. InputEndNumberInit .. " Cash, " .. TokenBoughtValue .. " Tokens gekauft."  ))
-                net.Send(ply)
-            end
+        --local CurrCash = ply:getDarkRPVar("money")
+        if not ply:canAfford(InputEndToTokensNumber) then
+            DarkRP.notify(ply, 1, 4, string.format("Das kannst du dir nicht leisten."))
+        else
+            ply:addMoney(-InputEndToTokensNumber)
+            net.Start("BackToken")
+            net.WriteUInt(TokenBoughtValue, 8)
+            DarkRP.notify(ply, 0, 4, string.format("Du hast dir für " .. InputEndToTokensNumber .. " Cash, " .. TokenBoughtValue .. " Tokens gekauft."))
+            net.Send(ply)
+        end
+    end)
+
+    net.Receive("ExchangeToCash", function(len, ply)
+        local InputEndToCashNumber = net.ReadUInt(8)
+        local OutPay = InputEndToCashNumber / 10
+        print(InputEndToCashNumber .. "")
+
+        if InputEndToCashNumber < 10 then
+            DarkRP.notify(ply, 1, 4, string.format("Die Zahl muss größer als 10 sein."))
+        else
+            DarkRP.notify(ply, 0, 4, string.format("Du hast dir für " .. InputEndToCashNumber .. " Tokens, " .. OutPay .. " Cash auszahlen lassen."))
+            ply:addMoney(OutPay)
         end
     end)
 end
